b6c2ff9c1293941e9ffc4917c325f0d94dca1b7c
azrinaldi <dickoazri791@gmail.com>
2023-08-10 13:26:00 +0700

    Update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,6 +7,14 @@ class Home extends BaseController
     public function index()
     {
         $data['title'] = 'Beranda';
+        $BangunanModel = new \App\Models\BangunanModel();
+        $data['getData'] = $BangunanModel->findAll();
         return view('Home/index_view', $data);
+    }
+    public function tampilkan_bangunan(){
+        $data['title'] = 'Beranda';
+        $BangunanModel = new \App\Models\BangunanModel();
+        $data['getData'] = $BangunanModel->findAll();
+        return view('Home/index_view', $data);   
     }
 }
--- app/Views/Home/index_view.php
+++ app/Views/Home/index_view.php
@@ -1,6 +1,7 @@
 <?= $this->extend('_layouts/index_view.php') ?>
 
 <?= $this->section('content') ?>
+
 <div id="map"></div>
 <?= $this->endSection() ?>
 <?= $this->section('style') ?>
@@ -37,38 +38,15 @@
         "Google Map": googleStreets,
         "OpenStreetMap": osm
     };
-    var geojsonFeature = {
-        "type": "MultiPolygon",
-        "bbox": [100.42654053037614, -0.47338084927683355, 100.42666176229999, -0.47320422190968103],
-        "coordinates": [
-            [
-                [
-                    [100.42654134465674, -0.4732062804182664],
-                    [100.42654098811813, -0.4732064001433094],
-                    [100.42654134359283, -0.4732063936023739],
-                    [100.42654134465674, -0.4732062804182664]
-                ]
-            ],
-            [
-                [
-                    [100.42654134359283, -0.4732063936023739],
-                    [100.42654053037614, -0.47329290808280217],
-                    [100.4265512275122, -0.4732941023799219],
-                    [100.42655243378344, -0.47338084927683355],
-                    [100.42659522061605, -0.4733772508772706],
-                    [100.42659462463249, -0.473368875414284],
-                    [100.42662968703216, -0.47337066292018865],
-                    [100.42662551160228, -0.4732946852520973],
-                    [100.42666176229999, -0.47329527600055393],
-                    [100.42665936660714, -0.47320422190968103],
-                    [100.42654134359283, -0.4732063936023739]
-                ]
-            ]
-        ]
-    };
-    var myLayer = L.geoJSON().addTo(map);
-    myLayer.addData(geojsonFeature);
-
-    L.control.layers(baseLayers).addTo(map);
+        
+        <?php foreach ($getData as $value) : ?>
+            var geojsonFeature = <?= $value->geojson ?>;
+            var bangunan = L.geoJSON(geojsonFeature).addTo(map);
+        <?php endforeach ?>
+        var overlays = {
+            "Marker" :singleMarker,
+            "PolygonData": bangunan
+        };
+        L.control.layers(baseLayers, overlays).addTo(map);
 </script>
 <?= $this->endSection() ?>
